8247f61d4f890cdb5828fefcea80638ba82a96e4
Nicholas Sledgianowski <nick@sledgeworx.io>
2021-11-06 17:04:21 -0700

    Split tests up

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,42 +4,42 @@ require 'capybara'
 require 'selenium-webdriver'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
  
-
-
-
-
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app,
-    :browser => :chrome,
-    :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
-       'chromeOptions': {
-                      'prefs': {
-                          'registered_protocol_handlers' => [{
-                            'default' => true,
-                            'protocol' => "mailto",
-                            'title' => 'Gmail',
-                            'url' => 'https://mail.google.com/mail/?extsrc=mailto&url=%s'
-                          }]
-                      }
-                  }
-    )
-  )
 end
 
-Capybara.default_driver = :chrome
 
-end
- # capabilities = {
- #    'chromeOptions': {
- #                      'prefs': {
- #                          'registered_protocol_handlers' => [{
- #                            'default' => true,
- #                            'protocol' => "mailto",
- #                            'title' => 'Gmail',
- #                            'url' => 'https://mail.google.com/mail/?extsrc=mailto&url=%s'
- #                          }]
- #                      }
- #                  }
- #    }
+
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app,
+#     :browser => :chrome,
+#     :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome(
+#        'chromeOptions': {
+#                       'prefs': {
+#                           'registered_protocol_handlers' => [{
+#                             'default' => true,
+#                             'protocol' => "mailto",
+#                             'title' => 'Gmail',
+#                             'url' => 'https://mail.google.com/mail/?extsrc=mailto&url=%s'
+#                           }]
+#                       }
+#                   }
+#     )
+#   )
+# end
+
+# Capybara.default_driver = :chrome
+
+# end
+#  # capabilities = {
+#  #    'chromeOptions': {
+#  #                      'prefs': {
+#  #                          'registered_protocol_handlers' => [{
+#  #                            'default' => true,
+#  #                            'protocol' => "mailto",
+#  #                            'title' => 'Gmail',
+#  #                            'url' => 'https://mail.google.com/mail/?extsrc=mailto&url=%s'
+#  #                          }]
+#  #                      }
+#  #                  }
+#  #    }
--- test/system/index_test.rb
+++ test/system/index_test.rb
@@ -3,27 +3,26 @@ require "selenium-webdriver"
 
 class DocsTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
-  # Make the Capybara DSL available in all integration tests
 
-  test "visiting the docs page" do
-    user = users(:two)
+  test "visiting the docs page while logged in" do
+    user = users(:one)
     sign_in user
     visit '/'
 
     element = page.find_link(:text => "Contact Us")
 
     href = element['href']
-    assert href.include?(user.email)
-    assert href.include?(user.id.to_s)
-
+    assert_includes href, "mailto:info@example.com"
+    assert_includes href, user.email
+    assert_includes href, user.id.to_s
     click_on "Contact Us"
-    take_screenshot
   end
 
 
-  # Reset sessions and driver between tests
-  teardown do
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
+  test "visiting the docs page" do 
+    visit '/' 
+    element = page.find_link(:text => "Contact Us") 
+    href = element['href'] 
+    assert href.include?("mailto:info@example.com") 
   end
 end
